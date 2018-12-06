910c78ae9b10a79cbae3d204471477703a681d4a
dyhorowitz@vassar.edu <dyhorowitz@sp309-185.cs.vassar.edu>
2018-12-06 18:52:52 -0500

    found simpler RLE implication

--- my_zip.c
+++ my_zip.c
@@ -11,51 +11,41 @@
 FILE *fp;
 
 
-/*RLECompress algorithm as written at
-http://paulbourke.net/dataformats/compress/rle.c*/
-
-char* RLECompress(unsigned char *output,unsigned char *input,int length)
-{
-   int count = 0,index,i;
-   unsigned char pixel;
-   int out = 0;
-
-   while (count < length) {
-      index = count;
-      pixel = input[index++];
-      while (index < length && index - count < 127 && input[index] == pixel)
-         index++;
-      if (index - count == 1) {
-         /* 
-            Failed to "replicate" the current pixel. See how many to copy.
-            Avoid a replicate run of only 2-pixels after a literal run. There
-            is no gain in this, and there is a risK of loss if the run after
-            the two identical pixels is another literal run. So search for
-            3 identical pixels.
-         */
-         while (index < length && index - count < 127
-               && (input[index] != input[index-1]
-               || index > 1 && input[index] != input[index-2]))
-            index++;
-         /* 
-            Check why this run stopped. If it found two identical pixels, reset
-            the index so we can add a run. Do this twice: the previous run
-            tried to detect a replicate run of at least 3 pixels. So we may be
-            able to back up two pixels if such a replicate run was found.
-         */
-         while (index < length && input[index] == input[index-1])
-            index--;
-         output[out++] = (unsigned char)(count - index);
-         for (i=count;i<index;i++)
-            output[out++] = input[i];
-      } else {
-         output[out++] = (unsigned char)(index - count);
-         output[out++] = pixel;
-      } /* if */
-      count=index;
-   } /* while */
-   return(output);
-}
+/* Returns the Run Length Encoded string for the  
+   source string src */
+char* RLECompress(char* src) 
+{ 
+    int rLen; 
+    int len = strlen(src); 
+  
+    /* If all characters in the source string are different,  
+    then size of destination string would be twice of input string. 
+    For example if the src is "abcd", then dest would be "a1b1c1d1" 
+    For other inputs, size would be less than twice.  */
+    char* dest = (char*)malloc(sizeof(char) * (len * 2 + 1)); 
+  
+    int i, j = 0; 
+  
+    /* traverse the input string one by one */
+    for (i = 0; i < len; i++) { 
+  
+        /* Count the number of occurrences of the new character */
+        rLen = 1; 
+        while (src[i] == src[i + 1]) { 
+            rLen++; 
+            i++; 
+        } 
+  
+        dest[j] = src[i];
+        j++;
+        dest[j] = rLen;
+        j++;
+    } 
+  
+    /*terminate the destination string */
+    dest[j] = '\0'; 
+    return dest; 
+} 
 
 void argument_overload_error(int argc, char *argv[]){
 	fprintf(stderr, "Error: Unexpected arguments: ");
@@ -72,17 +62,17 @@ void file_not_found_error(const char *filename){
 
 int main(int argc, char *argv[]) {
 	if(argc > 2){argument_overload_error(argc, argv);}
+
 	if(access(argv[1], F_OK) != 0){file_not_found_error(argv[1]);}
 
 	fp = fopen(argv[1], "r");
 	fseek(fp, 0L, SEEK_END);
 	char input[ftell(fp)];
-	char output[(2 * ftell(fp))];
 	rewind(fp);
 	fread(input, 5, sizeof(input), fp);
-	char *zip_output = RLECompress(output, input, (sizeof(input)));
+	char* output = RLECompress(input);
 
-	fwrite(zip_output, 5, sizeof(zip_output), stdout);
+	fwrite(output, 5, sizeof(output), stdout);
     printf("\n");
 	fclose(fp);
 }
