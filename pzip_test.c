73758aa5ad834179b0491e9d9b9b28282660add9
dyhorowitz@vassar.edu <dyhorowitz@sp309-185.cs.vassar.edu>
2018-12-15 13:49:13 -0500

    new test

---
+++ pzip_test.c
@@ -0,0 +1,172 @@
+/*********************************************************
+ * Your Name: Wilson Housen
+ * Partner Name: Dylan Horowitz
+ *********************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/sysinfo.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <pthread.h>
+#include <time.h>
+#include <semaphore.h> 
+
+//Including thread.h file from previous thread lab
+#include "thread.h"
+
+int fd;
+int file_size;
+struct stat sb;
+pthread_t prod_tid[3];
+pthread_t cons_tid;
+
+struct dataIn {
+    char* stack;
+    char* input;
+    int done;
+    int ticket;
+    int current_stack_element;
+    int on_stack;
+
+    sem_t mutex_sem;
+    sem_t producer_sem; 
+    struct lock producer_mutex;
+    struct lock consumer_mutex;
+    struct condition producer_cond;
+    struct condition consumer_cond;
+};
+
+#define handle_error(en, msg) \
+        do {errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+void
+dataIn_init(struct dataIn *dataIn){
+    printf("File_size: %d \n", file_size);
+    dataIn->stack = malloc(file_size);
+    dataIn->input = mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0);
+    dataIn->done = 0;
+    dataIn->ticket = 0;
+    dataIn->current_stack_element = 0;
+    dataIn->on_stack = 0;
+    lock_init(&dataIn->producer_mutex);
+    lock_init(&dataIn->consumer_mutex);
+    cond_init(&dataIn->producer_cond);
+    cond_init(&dataIn->consumer_cond);
+    sem_init(&dataIn->mutex_sem, 0, 1);
+    sem_init(&dataIn->producer_sem, 0, 1);
+};
+
+char* RLECompress(char* src) 
+{ 
+    int rLen; 
+    int len = strlen(src); 
+  
+    char* dest = (char*)malloc(len * 5);
+  
+    int i, j = 0; 
+  
+    /* traverse the input string one by one */
+    for (i = 0; i < len; i++) { 
+  
+        /* Count the number of occurrences of the new character */
+        rLen = 1; 
+        while (src[i] == src[i + 1]) { 
+            rLen++; 
+            i++; 
+        } 
+  
+        dest[j] = (char) rLen;
+        j = j + 4;
+        dest[j] = src[i];
+        j = j + 1;
+    } 
+    return dest;
+}
+
+void*
+producer(void *arg) {
+    struct dataIn *dataIn = (struct dataIn*)arg;
+    printf("Producer reached \n");
+    sem_post(&dataIn->producer_sem);
+    printf("Exiting producer \n");
+    return NULL;
+};
+
+void*
+consumer(void *arg) {
+    struct dataIn *dataIn = (struct dataIn*)arg;
+    printf("Consumer reached \n");
+    return NULL;
+};
+
+void argument_overload_error(int argc, char *argv[]){
+	fprintf(stderr, "Error: Unexpected arguments: ");
+	for(int x = 2; x < argc; x++){fprintf(stderr, " %s ", argv[x]);}
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+void file_not_found_error(const char *filename){
+	fprintf(stderr, "Error: file %s not found. \n", filename);
+	exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    if(argc > 2){argument_overload_error(argc, argv);}
+
+    if(access(argv[1], F_OK) != 0){file_not_found_error(argv[1]);}
+
+    fd = open(argv[1], O_RDONLY);
+    if(fstat(fd, &sb) == -1)
+    {fprintf(stderr, "Problem reading file. \n"); exit(1);}
+    file_size = sb.st_size;
+    struct dataIn *dataIn;
+    dataIn = malloc(sizeof(*dataIn));
+    dataIn_init(dataIn);
+
+    if(pthread_create(&cons_tid, NULL, &consumer, &dataIn) != 0)
+    {
+    	    fprintf(stderr, "Failed to create consumer thread. \n");
+    	    exit(1);
+    }
+
+    printf("Creates consumer thread \n");
+
+    printf("Start of producer loop \n");
+
+
+    int j = 0;
+    for(int i = 0; i < file_size; i++){
+	    printf("In producer for loop \n");
+            sem_wait(&dataIn->producer_sem);
+            if(pthread_create(&prod_tid[0], NULL, &producer, &dataIn) != 0)
+	    {
+	    	fprintf(stderr, "Failed to create producer thread. \n");
+	    	exit(1);
+	    }
+    }
+
+    printf("About to join. \n");
+
+    if (pthread_join(prod_tid[0], NULL) != 0)
+        {fprintf(stderr, "Failure in pthread_join for producer thread. \n");
+        exit(1);}
+    dataIn->done = 1;
+    printf("About to join 2 \n");
+    if (pthread_join(cons_tid, NULL) != 0)
+        {fprintf(stderr, "Failure in pthread_join for producer thread. \n");
+        exit(1);}
+    free(&dataIn->stack);
+    munmap(dataIn->input, file_size);
+    free(dataIn);
+
+    close(fd);
+    printf("Done! \n"); 
+
+}
