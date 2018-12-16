a1887a3883722fb8d0c4eca99f64f8f868e105e5
dyhorowitz@vassar.edu <dyhorowitz@sp309-185.cs.vassar.edu>
2018-12-16 12:12:29 -0500

    all code written draft 1

--- pzip_test.c
+++ pzip_test.c
@@ -25,13 +25,16 @@ int file_size;
 struct stat sb;
 pthread_t prod_tid[3];
 pthread_t cons_tid;
+struct dataIn *dataIn;
 
+//Struct passed to threads
 struct dataIn {
     char* stack;
     char* input;    
     char* prod_chunk;
     int done;
     int ticket;
+    int now_serving;
     int current_stack_element;
     int on_stack;
 
@@ -46,20 +49,24 @@ struct dataIn {
 
 void
 dataIn_init(struct dataIn *dataIn){
-    printf("File_size: %d \n", file_size);
+    //Initialize struct char* arrays
     dataIn->stack = malloc(file_size);
     dataIn->prod_chunk = malloc(10);
     dataIn->input = mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0);
+    //Initialize struct ints
     dataIn->done = 0;
     dataIn->ticket = 0;
+    dataIn->now_serving = 0;
     dataIn->current_stack_element = 0;
     dataIn->on_stack = 0;
+    //Initialize struct locks and condition variables
     lock_init(&dataIn->producer_mutex);
     lock_init(&dataIn->consumer_mutex);
     cond_init(&dataIn->producer_cond);
     cond_init(&dataIn->consumer_cond);
-    sem_init(&dataIn->mutex_sem, 0, 1);
-    sem_init(&dataIn->producer_sem, 0, 3);
+    //Initialize struct semaphores 
+    if(sem_init(&dataIn->mutex_sem, 0, 1) == -1){exit(-1);}
+    if(sem_init(&dataIn->producer_sem, 0, 3) == -1){exit(-1);}
 };
 
 char* RLECompress(char* src) 
@@ -91,17 +98,75 @@ char* RLECompress(char* src)
 
 void*
 producer(void *arg) {
-    printf("Entered producer \n");
     struct dataIn *dataIn = (struct dataIn*)arg;
-    if(sem_post(&dataIn->producer_sem) != 0){exit(1);}
-    printf("Exiting producer \n");
+    //Acquire ticket and data chunk to compress
+    lock_acquire(&dataIn->producer_mutex);
+    int my_ticket = dataIn->ticket;
+    dataIn->ticket++;
+    char* src = dataIn->prod_chunk;
+    lock_release(&dataIn->producer_mutex);
+    //Compress given data
+    src = RLECompress(src);
+    //Make sure to place the data in the proper location
+    while(my_ticket != dataIn->now_serving)
+    {   
+        cond_wait(&dataIn->producer_cond, &dataIn->producer_mutex);
+    }
+    //Place compressed data in stack and let next data chunk be placed
+    dataIn->stack[my_ticket] = src;
+    dataIn->now_serving++;
+    cond_signal(&dataIn->producer_cond, &dataIn->producer_mutex);
+    cond_signal(&dataIn->consumer_cond, &dataIn->consumer_mutex);
+    sem_post(&dataIn->producer_sem);
     return NULL;
 };
 
 void*
 consumer(void *arg) {
-    struct dataIn *dataIn = (struct dataIn*)arg;
-    printf("Consumer reached \n");
+    struct dataIn *dataIn = (struct dataIn*)arg;);
+    //Allow for Consumer thread to continue running until the end of the program
+    while(dataIn->done != 0){
+        //Check for the proper number of elements on the stack
+        while(dataIn->on_stack < 2){
+            cond_wait(&dataIn->consumer_cond, &dataIn->consumer_mutex);
+        }
+        if(dataIn->done != 0){
+            sem_wait(&dataIn->mutex_sem);
+            //Cutoff correction
+            char* first = dataIn->stack[0];
+            char* second = dataIn->stack[1];
+            if(first[(sizeof(first) - 2)] == second[0]){
+                int first_last = first[(sizeof(first) - 1)] - '0';
+                int second_first = second[1] - '0'
+                first[sizeof(first) - 1] = (char) (first_last + second_first);
+                memcpy(second, (second + 5), (sizeof(second) - 5));
+                dataIn->stack[1] = second;
+            }
+            fwrite(first, sizeof(first), 1, stdout);
+            //Move every stack element up 1
+            memcpy(dataIn->stack, (dataIn->stack + 1), sizeof(stack + 1));
+            dataIn->on_stack--;
+            sem_post(&dataIn->mutex_sem);
+        }
+        
+    }
+    //Once producers have finished, consumer runs on every element of the stack
+    //left
+    while(dataIn->on_stack > 1){
+        char* first = dataIn->stack[0];
+        char* second = dataIn->stack[1];
+        if(first[(sizeof(first) - 2)] == second[0]){
+            int first_last = first[(sizeof(first) - 1)] - '0';
+            int second_first = second[1] - '0'
+            first[sizeof(first) - 1] = (char) (first_last + second_first);
+            memcpy(second, (second + 5), (sizeof(second) - 5));
+            dataIn->stack[1] = second;
+        }
+        fwrite(chunk, sizeof(chunk), 1, stdout); 
+        memcpy(dataIn->stack, (dataIn->stack + 1), sizeof(stack + 1));
+        dataIn->on_stack--;
+    }
+    fwrite(dataIn->stack[0], sizeof(dataIn->stack[0]), 1, stdout);
     return NULL;
 };
 
@@ -118,6 +183,7 @@ void file_not_found_error(const char *filename){
 }
 
 int main(int argc, char *argv[]) {
+    //Check for proper number of arguments and that the file given can be found
     if(argc > 2){argument_overload_error(argc, argv);}
 
     if(access(argv[1], F_OK) != 0){file_not_found_error(argv[1]);}
@@ -126,32 +192,39 @@ int main(int argc, char *argv[]) {
     if(fstat(fd, &sb) == -1)
     {fprintf(stderr, "Problem reading file. \n"); exit(1);}
     file_size = sb.st_size;
-    struct dataIn *dataIn;
+
+    //Initialize data struct
     dataIn = malloc(sizeof(*dataIn));
     dataIn_init(dataIn);
 
-    if(pthread_create(&cons_tid, NULL, &consumer, &dataIn) != 0)
+    //Create consumer thread, which will run continuously 
+    if(pthread_create(&cons_tid, NULL, consumer, &dataIn) != 0)
     {
     	    fprintf(stderr, "Failed to create consumer thread. \n");
     	    exit(1);
     }
 
-    memcpy(dataIn->prod_chunk, (dataIn->input + 1), 10);
-
     int j = 0;
-    for(int i = 0; i < file_size; i++){
-	    printf("In producer for loop \n");
+    for(int i = 0; i < file_size; i += 2){
+            lock_acquire(&dataIn->producer_mutex);
+            //Read set number of bytes from input file into prod_chunk, 
+            //starting at offset i
+            memcpy(dataIn->prod_chunk, (dataIn->input + i), 2);
+            lock_release(&dataIn->producer_mutex);
             sem_trywait(&dataIn->producer_sem);
-            if(pthread_create(&prod_tid[j], NULL, &producer, &dataIn) != 0)
+            if(pthread_create(&prod_tid[j], NULL, producer, &dataIn) != 0)
 	    {
 	    	fprintf(stderr, "Failed to create producer thread. \n");
 	    	exit(1);
 	    }
         if(j == 2){j = 0;}
         else j++;
+        
     }
 
 
+    //Make sure all Producer threads finish executing
+    //Last parts of input file
     if (pthread_join(prod_tid[0], NULL) != 0)
         {fprintf(stderr, "Failure in pthread_join for producer thread. \n");
         exit(1);}
@@ -161,16 +234,18 @@ int main(int argc, char *argv[]) {
     if (pthread_join(prod_tid[2], NULL) != 0)
         {fprintf(stderr, "Failure in pthread_join for producer thread. \n");
         exit(1);}
+
+    sem_close(&dataIn->producer_sem);
     dataIn->done = 1;
-    printf("About to join 2 \n");
+    //Allow for consumer thread to finish writing compressed data to stdout
     if (pthread_join(cons_tid, NULL) != 0)
         {fprintf(stderr, "Failure in pthread_join for producer thread. \n");
         exit(1);}
-    free(&dataIn->stack);
+
+    //Clean up
     munmap(dataIn->input, file_size);
+    sem_close(&dataIn->mutex_sem);
     free(dataIn);
 
-    close(fd);
-    printf("Done! \n"); 
-
+    close(fd); 
 }
